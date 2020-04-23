ceb5441295424e6991957647bba87fccbd85211c
Dimas Kotvan <dimas.kotvan@gmail.com>
2020-04-23 08:35:50 -0300

    Neovim alias

--- alias.zsh
+++ alias.zsh
@@ -5,3 +5,6 @@ alias prl="hub pr list"
 alias pro="hub pr show"
 alias prco="hub pr checkout"
 
+# NVIM
+alias update_nvim='nvim -c "try | call dein#update() | messages | finally | qall! | endtry" --headless'
+alias ngst='nvim -c ":G"'
