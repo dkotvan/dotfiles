02b0011eb4f095aecc3e13dbd24ea85b9aa0e149
Dimas Kotvan <dimas.kotvan@ifood.com.br>
2020-05-24 16:54:52 -0300

    better git aliases

--- alias.zsh
+++ alias.zsh
@@ -1,8 +1,13 @@
 # Git aliases
-alias prci='xdg-open `hub ci-status -f %U`'
+if [[ $OSTYPE == darwin* ]]; then
+  alias prci='xdg-open `hub ci-status -f %U`'
+else
+  alias prci='xopen `hub ci-status -f %U`'
+fi
 alias prl="hub pr list"
 alias pro="hub pr show"
 alias prco="hub pr checkout"
+alias brdiff="git difftool --dir-diff --tool=vimdirdiff"
 
 # NVIM
 alias update_nvim='nvim -c "try | call dein#update() | messages | finally | qall! | endtry" --headless'
