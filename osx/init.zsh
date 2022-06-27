3d72cb54590b36691ee08e8350937dedc56b9a36
Dimas Kotvan <dimas.kotvan@ifood.com.br>
2022-06-27 09:27:05 -0300

    Add GraalVM config

--- osx/init.zsh
+++ osx/init.zsh
@@ -1,5 +1,6 @@
 # Load Java 17 - java 18 is still messy
 export JAVA_HOME=`/usr/libexec/java_home -v 17`
+export GRAALVM_HOME=`/usr/libexec/java_home -V 2>&1 | grep GraalVM | sed -r 's/^.+(\/Library.+)$/\1/'`
 
 # Fix problem with Packer nvim plugin trying to update a lot of plugins at the same time
 ulimit -S -n 200048
@@ -9,3 +10,4 @@ if [ ! -d "~/.docker/cli-plugins/docker-compose" ]; then
   mkdir -p ~/.docker/cli-plugins
   ln -sfh $(brew --prefix)/opt/docker-compose ~/.docker/cli-plugins/docker-compose
 fi
+
