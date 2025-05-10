dd11c78ad361fe5bad26689ece2a0b7c6d182ab2
Dimas Kotvan <dimas.kotvan@ifood.com.br>
2025-05-10 21:48:22 +0100

    Add lazyvim

---
+++ lazyvim/lua/plugins/override.lua
@@ -0,0 +1,16 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+
+  -- Piin mason om v1
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
