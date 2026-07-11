471b114853a7045cf782c8a4049771704f63e88f
Dimas Kotvan <dimas.kotvan@gmail.com>
2026-07-11 15:57:09 +0100

    fix auto complete

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -189,7 +189,7 @@ return {
 		version = "*",
 		dependencies = "rafamadriz/friendly-snippets",
 		opts = {
-			keymap = { preset = "default" },
+			keymap = { preset = "super-tab" },
 			appearance = {
 				use_nvim_cmp_as_default = true,
 				nerd_font_variant = "mono",
